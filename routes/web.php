f61480715c78806ac2e06508f64762b5bb1ee196
victor john <victormatalanza93@gmail.com>
2022-07-07 11:31:26 +0300

    managing routes for he project

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [App\Http\Controllers\ShortenerController::class, 'index'])->middleware(['auth'])->name('dashboard');
+Route::post('/dashboard', [App\Http\Controllers\ShortenerController::class, 'store'])->middleware(['auth'])->name('url.shorten');
+Route::post('/dashboard/{id}', [App\Http\Controllers\ShortenerController::class, 'update'])->middleware(['auth'])->name('update');
+Route::get('/delete_url/{id}', [App\Http\Controllers\ShortenerController::class, 'destroy'])->middleware(['auth'])->name('url.delete');
 
 require __DIR__.'/auth.php';
